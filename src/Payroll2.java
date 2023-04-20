9ca290723d3553c468e5ded9f51cc99e73d01344
ariful2000 <57025439+ariful2000@users.noreply.github.com>
2023-04-20 02:08:02 -0400

    added hello message 4

--- src/Payroll2.java
+++ src/Payroll2.java
@@ -38,5 +38,9 @@ public class Payroll2 {
         System.out.println("Tax deducted: " + deductions);
 
         System.out.println("Net pay: " + net_pay);
+
+        System.out.println("Hello world 3");
+
+        System.out.println("Hello world 4");
     }
 }
