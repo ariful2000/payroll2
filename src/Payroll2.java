ee980d0fd364d1921c5297a874b8129f6faa965d
ariful2000 <57025439+ariful2000@users.noreply.github.com>
2023-04-20 01:14:27 -0400

    Payroll2 Project

---
+++ src/Payroll2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Payroll2 {
+    public static void main(String[] args) {
+
+        String firstName, lastName;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter employee's first name: ");
+        firstName = input.nextLine();
+
+        System.out.println("Enter employee's last name: ");
+        lastName = input.nextLine();
+
+        System.out.println("Enter number of hours " + firstName.toUpperCase() + " " + lastName.toUpperCase() + " worked:");
+        double  hours = input.nextDouble();
+
+        System.out.println("Enter hourly rate of pay:");
+        double  pay = input.nextDouble();
+
+        double  gross_pay = pay * hours;
+
+        System.out.println("Enter federal tax rate:");
+        double  fedTax = input.nextDouble();
+        double fedTaxRate = fedTax;
+
+        double deductions = fedTaxRate/100 * gross_pay;
+
+        double net_pay = gross_pay - deductions;
+
+        System.out.println("Hours worked: " + hours);
+
+        System.out.println("Hourly rate: " + pay);
+
+        System.out.println("Gross pay: " + gross_pay);
+
+        System.out.println("Tax deducted: " + deductions);
+
+        System.out.println("Net pay: " + net_pay);
+    }
+}
